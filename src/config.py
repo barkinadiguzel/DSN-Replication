334acfd75cf5c0d094d52bea74e420fff89f65fd
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-23 11:31:42 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,5 @@
+# Hyperparameters for DSN
+NUM_HIDDEN_LAYERS = 3
+NUM_CLASSES = 10
+ALPHA_LIST = [0.3, 0.3, 0.4]  # Weights for companion losses
+GAMMA = 0.01                  # Threshold for companion loss
